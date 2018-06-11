b6ec721d9912b2b804d4ba66e7fb23ea4d863fa4
hanran <hanran@imagedt.com>
2018-06-11 13:13:16 +0800

    Add tf test nn model

---
+++ imagedt/tensorflow/network/lenet.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+from __future__ import absolute_import
+from __future__ import print_function
+
+import tensorflow as tf
+
+slim = tf.contrib.slim 
+
+def lenet(images, num_classes=10, is_training=True, drop_keep_prob=0.5,
+          prediction_fn=slim.softmax, scope='LeNet'):
+  """
+  Args: 
+    images: A batch of `Tensors` of size [batch_size, height, width, channels].
+    num_classes: classes of the training dataset
+    drop_keep_prob: set drop out prob
+  """
+  end_points = {}
+
+  with tf.variable_scope(scope, 'LeNet', [images]):
+    net = end_points['conv1'] = slim.conv2d(images, 32, [5, 5], scope='conv1')
+    net = end_points['pool1'] = slim.max_pool2d(net, [2, 2], 2, scope='pool1')
+    net = end_points['conv2'] = slim.conv2d(net, 64, [5, 5], scope='conv2')
+    net = end_points['pool2'] = slim.max_pool2d(net, [2, 2], 2, scope='pool2')
+    net = slim.flatten(net)
+    end_points['Flatten'] = net
+
+    net = end_points['fc3'] = slim.fully_connected(net, 1024, scope='fc3')
+
+    if not num_classes:
+      return net, end_points
+
+    net = end_points['dropout3'] = slim.dropout(net, drop_keep_prob, is_training=is_training,
+          scope='dropout3')
+
+    logits = end_points['Logits'] = slim.fully_connected(
+            net, num_classes, activation_fn=None, scope='fc4')
+
+    net = end_points['Predictions'] = prediction_fn(logits, scope='Predictions')
+
+    return logits, end_points
+lenet.default_image_size = 28
+
+
+
---
+++ imagedt/tensorflow/network/test_lenet.py
@@ -0,0 +1,115 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for slim.nets.vgg."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import tensorflow as tf
+
+import lenet
+
+slim = tf.contrib.slim
+
+
+class LeNetTest(tf.test.TestCase):
+  print("init test: LeNetTest...")
+
+  def testBuild(self):
+    batch_size = 5
+    height, width = 28, 28
+    num_classes = 10
+    with self.test_session():
+      inputs = tf.random_uniform((batch_size, height, width, 3))
+      logits, _ = lenet.lenet(inputs, num_classes)
+      self.assertEquals(logits.op.name, 'LeNet/fc4/BiasAdd')
+      self.assertListEqual(logits.get_shape().as_list(),
+                           [batch_size, num_classes])
+
+  def testFullyConvolutional(self):
+    batch_size = 1
+    height, width = 28, 28
+    num_classes = 10
+    with self.test_session():
+      inputs = tf.random_uniform((batch_size, height, width, 3))
+      logits, _ = lenet.lenet(inputs, num_classes)
+      self.assertEquals(logits.op.name, 'LeNet/fc4/BiasAdd')
+      self.assertListEqual(logits.get_shape().as_list(),
+                           [batch_size, num_classes])
+
+  def testGlobalPool(self):
+    batch_size = 1
+    height, width = 28, 28
+    num_classes = 10
+    with self.test_session():
+      inputs = tf.random_uniform((batch_size, height, width, 3))
+      logits, _ = lenet.lenet(inputs, num_classes)
+      self.assertEquals(logits.op.name, 'LeNet/fc4/BiasAdd')
+      self.assertListEqual(logits.get_shape().as_list(),
+                           [batch_size, num_classes])
+
+  def testEndPoints(self):
+    batch_size = 5
+    height, width = 28, 28
+    num_classes = 10
+    with self.test_session():
+      inputs = tf.random_uniform((batch_size, height, width, 3))
+      _, end_points = lenet.lenet(inputs, num_classes)
+      expected_names = ['conv1', 
+                        'pool1',
+                        'conv2', 
+                        'pool2', 
+                        'Flatten',
+                        'fc3',
+                        'dropout3', 
+                        'Logits', 
+                        'Predictions']
+
+      self.assertSetEqual(set(end_points.keys()), set(expected_names))
+
+  def testEvaluation(self):
+    batch_size = 2
+    height = width = lenet.lenet.default_image_size  # set train imgae size
+    num_classes = 10
+    with self.test_session():
+      eval_inputs = tf.random_uniform((batch_size, height, width, 3))
+      logits, _ = lenet.lenet(eval_inputs, is_training=False)
+      self.assertListEqual(logits.get_shape().as_list(),
+                           [batch_size, num_classes])
+      predictions = tf.argmax(logits, 1)
+      self.assertListEqual(predictions.get_shape().as_list(), [batch_size])
+
+  def testForward(self): 
+    batch_size = 1
+    height = width = lenet.lenet.default_image_size  # set train imgae size
+    with self.test_session() as sess:
+      inputs = tf.random_uniform((batch_size, height, width, 3))
+      logits, _ = lenet.lenet(inputs)
+      sess.run(tf.global_variables_initializer())
+      output = sess.run(logits)
+      self.assertTrue(output.any())
+
+  def test_train(self):
+    imgae_dir = '/Users/zhengyongzhe/Documents/imageDT-work/ImageDT/test/images'
+    images_path = os.listdir(imgae_dir)
+
+    for image_path in images_path:
+      cv2.imread(os.path.join(image_dir, image_path))
+
+
+
+if __name__ == '__main__':
+  tf.test.main()
