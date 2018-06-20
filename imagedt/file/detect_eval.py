0f62a51760e35ec0a219dc09433ca15cbbf6167b
hanran <hanran@imagedt.com>
2018-06-20 22:18:36 +0800

    add detect  map

---
+++ imagedt/file/detect_eval.py
@@ -0,0 +1,181 @@
+# coding: utf-8
+from __future__ import absolute_import
+from __future__ import print_function
+
+import xml.etree.ElementTree as ET
+import os
+import cPickle
+import numpy as np
+
+from .file_write import write_txt
+from ..dir.dir_loop import loop
+
+
+def parse_rec(filename):
+    """ Parse a PASCAL VOC xml file """
+    tree = ET.parse(filename)
+    objects = []
+    for obj in tree.findall('object'):
+        obj_struct = {}
+        obj_struct['name'] = obj.find('name').text
+        # obj_struct['pose'] = obj.find('pose').text
+        # obj_struct['truncated'] = int(obj.find('truncated').text)
+        obj_struct['difficult'] = 0
+        bbox = obj.find('bndbox')
+        obj_struct['bbox'] = [float(bbox.find('xmin').text),
+                              float(bbox.find('ymin').text),
+                              float(bbox.find('xmax').text),
+                              float(bbox.find('ymax').text)]
+        objects.append(obj_struct)
+
+    return objects
+
+def parse_detect(detannos):
+    xmls_path = loop(detannos, ['.xml'])
+
+    infos = []
+    confidence = 0.9
+    for xml_path in xmls_path:
+        info = parse_rec(xml_path)
+
+        basename = os.path.basename(xml_path)
+        infos.append([os.path.splitext(basename)[0], [item['bbox'] for item in info]])
+
+    return [[item[0], confidence, item[1]] for item in infos]
+
+
+def voc_ap(rec, prec, use_07_metric=False):
+    """ ap = voc_ap(rec, prec, [use_07_metric])
+    Compute VOC AP given precision and recall.
+    If use_07_metric is true, uses the
+    VOC 07 11 point method (default:False).
+    """
+    if use_07_metric:
+        # 11 point metric
+        ap = 0.
+        for t in np.arange(0., 1.1, 0.1):
+            if np.sum(rec >= t) == 0:
+                p = 0
+            else:
+                p = np.max(prec[rec >= t])
+            ap = ap + p / 11.
+    else:
+        # correct AP calculation
+        # first append sentinel values at the end
+        mrec = np.concatenate(([0.], rec, [1.]))
+        mpre = np.concatenate(([0.], prec, [0.]))
+
+        # compute the precision envelope
+        for i in range(mpre.size - 1, 0, -1):
+            mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+        # to calculate area under PR curve, look for points
+        # where X axis (recall) changes value
+        i = np.where(mrec[1:] != mrec[:-1])[0]
+
+        # and sum (\Delta recall) * prec
+        ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
+
+def get_images_basename(xml_path):
+    return [os.path.splitext(os.path.basename(item))[0] for item in loop(xml_path)]
+
+
+def voc_eval(detpath,
+             annopath,
+             classname,
+             ovthresh=0.5,
+             use_07_metric=False):
+
+    # read list of images
+    imagenames = get_images_basename(detpath)
+    # load gt
+    recs = {}
+    for i, imagename in enumerate(imagenames):
+        recs[imagename] = parse_rec(os.path.join(annopath, '{0}.xml').format(imagename))
+        print('Reading annotation for {:d}/{:d}'.format(
+            i + 1, len(imagenames)))
+
+    # extract gt objects for this class
+    class_recs = {}
+    npos = 0
+    for imagename in imagenames:
+        R = [obj for obj in recs[imagename] if obj['name'] == classname]
+        bbox = np.array([x['bbox'] for x in R])
+        difficult = np.array([x['difficult'] for x in R]).astype(np.bool)
+        det = [False] * len(R)
+        npos = npos + sum(~difficult)
+        class_recs[imagename] = {'bbox': bbox,
+                                 'difficult': difficult,
+                                 'det': det}
+
+    # read dets
+    if os.path.isfile(detpath):
+        detfile = detpath.format(classname)
+        with open(detfile, 'r') as f:
+            lines = f.readlines()
+        splitlines = [x.strip().split(' ') for x in lines]
+    else:
+        lines = parse_detect(detpath)
+        splitlines = [[item[0]]+[item[1]] + x for item in lines for x in item[2]]
+
+    image_ids = [x[0] for x in splitlines]
+    confidence = np.array([float(x[1]) for x in splitlines])
+    BB = np.array([[float(z) for z in x[2:]] for x in splitlines])
+
+    # sort by confidence
+    sorted_ind = np.argsort(-confidence)
+    sorted_scores = np.sort(-confidence)
+    BB = BB[sorted_ind, :]
+    image_ids = [image_ids[x] for x in sorted_ind]
+
+    # go down dets and mark TPs and FPs
+    nd = len(image_ids)
+    tp = np.zeros(nd)
+    fp = np.zeros(nd)
+    for d in range(nd):
+        R = class_recs[image_ids[d]]
+        bb = BB[d, :].astype(float)
+        ovmax = -np.inf
+        BBGT = R['bbox'].astype(float)
+
+        if BBGT.size > 0:
+            # compute overlaps
+            # intersection
+            ixmin = np.maximum(BBGT[:, 0], bb[0])
+            iymin = np.maximum(BBGT[:, 1], bb[1])
+            ixmax = np.minimum(BBGT[:, 2], bb[2])
+            iymax = np.minimum(BBGT[:, 3], bb[3])
+            iw = np.maximum(ixmax - ixmin + 1., 0.)
+            ih = np.maximum(iymax - iymin + 1., 0.)
+            inters = iw * ih
+
+            # union
+            uni = ((bb[2] - bb[0] + 1.) * (bb[3] - bb[1] + 1.) +
+                   (BBGT[:, 2] - BBGT[:, 0] + 1.) *
+                   (BBGT[:, 3] - BBGT[:, 1] + 1.) - inters)
+
+            overlaps = inters / uni
+            ovmax = np.max(overlaps)
+            jmax = np.argmax(overlaps)
+
+        if ovmax > ovthresh:
+            if not R['difficult'][jmax]:
+                if not R['det'][jmax]:
+                    tp[d] = 1.
+                    R['det'][jmax] = 1
+                else:
+                    fp[d] = 1.
+        else:
+            fp[d] = 1.
+
+    # compute precision recall
+    fp = np.cumsum(fp)
+    tp = np.cumsum(tp)
+    rec = tp / float(npos)
+    # avoid divide by zero in case the first detection matches a difficult
+    # ground truth
+    prec = tp / np.maximum(tp + fp, np.finfo(np.float64).eps)
+    ap = voc_ap(rec, prec, use_07_metric)
+
+    return rec, prec, ap
--- test/Test_file_operators.py
+++ test/Test_file_operators.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os
 import sys
 sys.path.append('./')
+
+
 
 from imagedt.file import parse_annotation, file_operate
 from imagedt.tensorflow.tools import data_converters
 from imagedt.dir import dir_loop
+from imagedt.image import process
+from imagedt.file import detect_eval
+from imagedt.file import file_write
+
 
 class Test_File_operators(object):
     def __init__(self):
         super(Test_File_operators, self).__init__()
         self.parse_tools = parse_annotation.Anno_OP()
         self.file_op = file_operate.FilesOp()
-        self.data_dir = 'test/sources/data_dir'
+        self.data_dir = '/data/dataset/shelf_datasets/liby_merge_shlef'
 
     def parse_annotations(self, xml_path):
         self.parse_tools.parse_lab_xml(xml_path)
@@ -23,7 +30,7 @@ class Test_File_operators(object):
         self.parse_annotations(xml_poaths[0])
 
     def test_converte_detect_records(self):
-        save_path = './test/sources/training.tfrecord'
+        save_path = '/data/dataset/shelf_datasets/liby_merge_shlef/training.tfrecord'
         data_converters.converte_detect_records(self.data_dir, save_path)
 
     def test_file_operators(self):
@@ -31,9 +38,32 @@ class Test_File_operators(object):
         self.file_op.del_broken_image(self.data_dir)
         self.file_op.check_data_pairs(self.data_dir)
 
+    def test_file_operate_one(self):
+        self.file_op.rename_class_dir('/data/dataset/liby_offline/train_renet50/zip', 'other_diao_')
+
+    def remove_brokken(self):
+        process.remove_broken_image('/data/dataset/liby_offline/train_renet50/zip')
+
+    def rename_xml_cls_name(self):
+        self.parse_tools.reset_xml_cls('/data/dataset/shelf_datasets/test_detect_save/Annotations')
+
+    def detect_eval_map(self):
+        detpath = '/data/dataset/shelf_datasets/test_jpg/Annotations'
+        annopath = '/data/dataset/shelf_datasets/test_jpg/Annotations2'
+
+        rec, prec, ap = detect_eval.voc_eval(detpath, annopath, '3488',
+                                                     ovthresh=0.5,
+                                                     use_07_metric=False)
+        return rec, prec, ap
+
 
 if __name__ == '__main__':
     File_operate_init = Test_File_operators()
-    File_operate_init.test_pars_annotations()
-    File_operate_init.test_converte_detect_records()
-    File_operate_init.test_file_operators()
+    # File_operate_init.test_pars_annotations()
+    # File_operate_init.test_file_operators()
+    # File_operate_init.test_converte_detect_records()
+    # File_operate_init.test_file_operate_one()
+    # File_operate_init.rename_xml_cls_name()
+    rec, prec, ap = File_operate_init.detect_eval_map()
+
+    print "mean ap: {0}".format(ap)
