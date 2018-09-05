19b12a27e4647029e4b6c5e5d305d8b0087e7897
Moyan <ice_moyan@163.com>
2018-09-05 19:08:23 +0800

    update tf model

--- imagedt/tensorflow/tools/tfmodel_wrapper.py
+++ imagedt/tensorflow/tools/tfmodel_wrapper.py
@@ -9,11 +9,11 @@ class TFmodel_Wrapper(object):
   """docstring for TFmodel_Wrapper"""
   def __init__(self, pbmodel_path, input_nodename='input', output_nodename='softmax', gpu_id=0, graph_name='classify'):
     super(TFmodel_Wrapper, self).__init__()
-    self.pbmodel_path = pbmodel_path
-    self.input_node = input_nodename
-    self.output_node = output_nodename.split(',')
-    self.gpu_id = gpu_id
     self.graph_name = graph_name
+    self.pbmodel_path = pbmodel_path
+    self.input_node = self.graph_name+'/'+input_nodename
+    self.output_node = self.graph_name+'/'+output_nodename
+    self.gpu_id = gpu_id
     self._load_model()
     self._check_node_name()
     self._set_output_node()
@@ -50,18 +50,12 @@ class TFmodel_Wrapper(object):
     # Get handles to input and output tensors
     self.tensor_dict = {}
     intput_tensor = self.input_node + ':0'
-    output_tensor = self.output_node + ':0'+','+self.output_node + ':0'
+    output_tensor = self.output_node + ':0'
     self.image_tensor = tf.get_default_graph().get_tensor_by_name(intput_tensor)
     self.tensor_dict[self.output_node] = tf.get_default_graph().get_tensor_by_name(output_tensor)
 
-<<<<<<< HEAD
-
   # from imagedt.decorator import time_cost
   # @time_cost
-=======
-  from imagedt.decorator import time_cost
-  @time_cost
->>>>>>> 4720af1a4252316dee63773e3315cb98c17a1b72
   def predict(self, images):
     # Run inference
     output_infos = self.sess.run(self.tensor_dict, feed_dict={self.image_tensor: images})[self.output_node]
